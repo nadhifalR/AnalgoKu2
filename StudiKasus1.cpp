ceb4b9fb23346bc7fa45735056b4d0c4ef4fd618
NadhifalR <nadhifal01@gmail.com>
2020-03-10 18:57:28 +0700

    commit

---
+++ StudiKasus1.cpp
@@ -0,0 +1,26 @@
+/*
+Nama    = Nadhifal A. Rendusara
+NPM     = 140810180048
+Kelas   = B
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=0;
+    int n=10;
+    int x[n]{1,20,12,41,10,32,41,22,92,70};
+    int maks = x[0];
+
+    do{
+        if(x[i]>maks){
+            maks=x[i];
+        }
+        i++;
+    }while(i<n);
+
+    cout<<"Maks : "<<maks;
+
+    return 0;
+}
